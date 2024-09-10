dd57f3f5d3a43d61abde6db28734a9132a6fcc06
LuisEduardo89 <luiseduardoherreratellez@gmail.com>
2024-09-10 18:46:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
+import random
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands
 
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+Bot = commands.Bot (command_prefix = "$" ,intents=intents)
+
+@Bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {Bot.user}')
+
+@Bot.command()
+async def hello(ctx):
+    await ctx.send("HI!")
+
+@Bot.command()
+async def bye(ctx):
+    await ctx.send("😔")
+
+@Bot.command()
+async def Help(ctx):
+    await ctx.send("$hello, $bye, $password, $help, $choose, $repeat")
+
+@Bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@Bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+    
+@Bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+Bot.run("MTI3ODE1Nzg2NTI0ODYyNDY5MQ.GF1YYQ.HLdto7jN7sUeOYVzZvT3JUT3VPfuSoI7zcBKd0")
